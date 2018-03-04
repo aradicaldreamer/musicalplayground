11008b40c92c2719c4572236197e45e1b86b5f8d
Billy <>
2018-03-04 15:58:32 +0000

    Tracking unity project

---
+++ ProjectionPrototype/Assets/Scripts/PositionReset.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionReset : MonoBehaviour {
+
+	Vector3 startPos;
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		startPos = transform.position;
+		rb = GetComponent<Rigidbody> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnCollisionEnter(Collision col) 
+	{
+		if (col.transform.tag == "clipping_floor") 
+		{
+			ResetPos();
+		}
+	}
+	void ResetPos() 
+	{
+		rb.velocity = Vector3.zero;
+		transform.position = startPos;
+	}
+}
---
+++ ProjectionPrototype/Assets/Scripts/RandomMaterialColour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMaterialColour : MonoBehaviour {
+	Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+		rend.material.color = Random.ColorHSV(0f,1f,0f,1f,0.5f,1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
