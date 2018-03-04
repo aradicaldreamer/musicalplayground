5aad95fe321deb41b96f6d635ba85b6182a8b3eb
Fletchbot <al-fletch@hotmail.co.uk>
2018-03-04 16:53:11 +0000

    Created basic note triggers by movement in alex scene

---
+++ ProjectionPrototype/Assets/Scripts/PianoTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoTrigger : MonoBehaviour 
+{
+	public AudioSource PianoSource;
+	public AudioClip NoteC;
+	public AudioClip NoteDb;
+	public AudioClip NoteE;
+	public AudioClip NoteEb;
+	public AudioClip NoteF;
+	public AudioClip NoteGb;
+	public AudioClip NoteG;
+	public AudioClip NoteAb;
+	public AudioClip NoteA;
+	public AudioClip NoteBb;
+	public AudioClip NoteB;
+
+
+	private bool hasPlayedAudio;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag ("Player") && hasPlayedAudio == false) {
+			PianoSource.PlayOneShot (NoteC);
+			PianoSource.PlayOneShot (NoteF);
+			hasPlayedAudio = false;
+
+		}
+	}
+}
