0fb7ee6b1eb2eaf4df50177929bae398dda44a3b
Billy - Macbook <wkarnchanapee@gmail.com>
2018-03-04 17:57:12 +0000

    audio trigger cubes

---
+++ ProjectionPrototype/Assets/AudioSwitch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSwitch : MonoBehaviour {
+
+	public AudioClip aud;
+	public AudioSource source;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter(Collider col) {
+		source.PlayOneShot(aud);
+	}
+}
