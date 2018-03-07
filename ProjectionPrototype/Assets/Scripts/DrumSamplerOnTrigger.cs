a13161a30b9e181db9cc4769cc100af943456e7f
Fletchbot <al-fletch@hotmail.co.uk>
2018-03-07 16:51:49 +0000

    Organised the asset folder and made test scenes for audio helm

---
+++ ProjectionPrototype/Assets/Scripts/DrumSamplerOnTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrumSamplerOnTrigger : MonoBehaviour {
+	public AudioHelm.Sampler sampler; //ref to helm controller to play sampler
+	public int note = 48; //Is the midi note 0 to 127 (should trigger kick on C3)
+
+	void OnCollisionEnter(Collision collision)
+	{
+		// int note, float velocity (how hard note is hit), float length (how long the note decays)
+		sampler.NoteOn(note); 
+	}
+}
---
+++ ProjectionPrototype/Assets/Scripts/SynthNoteOnTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SynthNoteOnTrigger : MonoBehaviour {
+
+	public AudioHelm.HelmController helmController; //ref to helm controller to play synth
+	public int note = 60; //Is the midi note 0 to 127
+	public float subVolume = 0.0f; // variable example to change a synth parameter
+
+	void OnCollisionEnter(Collision collision)
+	{
+		// Example of how to control a parameter from the standalone synth
+		helmController.SetParameterPercent(AudioHelm.Param.kSubVolume, subVolume);
+
+		// int note, float velocity (how hard note is hit), float length (how long the note decays)
+		helmController.NoteOn(note, 1.0f, 0.5f); 
+	}
+}
