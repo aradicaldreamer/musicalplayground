92b2b576fe8624789d9a0791fcf913c584b05e43
Fletchbot <al-fletch@hotmail.co.uk>
2018-03-12 13:36:54 +0000

    Added meldoy scene
    The cam needs to be high above, had issues at home

---
+++ ProjectionPrototype/Assets/Scripts/MelodyNoteOnTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MelodyNoteOnTrigger : MonoBehaviour {
+
+	public AudioHelm.HelmController helmController; //ref to helm controller to play synth
+	public int note = 60; //Is the midi note 0 to 127
+	void OnCollisionEnter(Collision collision)
+	{
+		// int note, float velocity (how hard note is hit), float length (how long the note decays)
+		helmController.NoteOn(note, 1.0f, 0.5f); 
+	}
+}
