5901a25cee083dfe9a29ac022d6bdf40fa26daa0
Fletchbot <al-fletch@hotmail.co.uk>
2018-03-12 19:06:21 +0000

    Added DrumTriggers scene

---
+++ ProjectionPrototype/Assets/Scripts/DrumTriggers.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrumTriggers : MonoBehaviour {
+	public AudioHelm.Sequencer sequencer;
+	public int note = 48;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void OnCollisionEnter(Collision collision)
+	{
+		int seqPos = (int)sequencer.GetSequencerPosition ();
+		sequencer.AddNote (note, seqPos, seqPos + 1);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
