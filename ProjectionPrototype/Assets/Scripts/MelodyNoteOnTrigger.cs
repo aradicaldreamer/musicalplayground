05671e91599c3f4163d010a46d835fe3d5b706eb
aradicaldreamer <31594961+aradicaldreamer@users.noreply.github.com>
2018-03-13 17:24:44 +0000

    Updated Collision Messages to appropriate triggers.

--- ProjectionPrototype/Assets/Scripts/MelodyNoteOnTrigger.cs
+++ ProjectionPrototype/Assets/Scripts/MelodyNoteOnTrigger.cs
@@ -6,9 +6,10 @@ public class MelodyNoteOnTrigger : MonoBehaviour {
 
 	public AudioHelm.HelmController helmController; //ref to helm controller to play synth
 	public int note = 60; //Is the midi note 0 to 127
-	void OnCollisionEnter(Collision collision)
-	{
+	void OnTriggerEnter(Collider col)
+	{	print ("collided with " + name.ToString ());
 		// int note, float velocity (how hard note is hit), float length (how long the note decays)
 		helmController.NoteOn(note, 1.0f, 0.5f); 
 	}
+		
 }
