548a1adbb2c892a8347fba8485787a9620380779
Fletchbot <al-fletch@hotmail.co.uk>
2018-03-15 00:50:11 +0000

    Editing soundscape room

--- ProjectionPrototype/Assets/AgentPathfinding.cs
+++ ProjectionPrototype/Assets/AgentPathfinding.cs
@@ -8,12 +8,10 @@ public class AgentPathfinding : MonoBehaviour {
 	public Transform target;
 	NavMeshAgent agent;
 
-
 	// Use this for initialization
 	void Start () {
 		agent = GetComponent<NavMeshAgent>();
 	}
-
 	// Update is called once per frame
 	void Update () {
 		agent.SetDestination(target.position);
--- ProjectionPrototype/Assets/Scripts/HelmManagerScript.cs
+++ ProjectionPrototype/Assets/Scripts/HelmManagerScript.cs
@@ -7,12 +7,14 @@ public class HelmManagerScript : MonoBehaviour {
 	public AudioHelm.HelmController Bass; //ref to helm controller to play synth
 	public AudioHelm.HelmController Arp;
 	public AudioHelm.HelmController AirDrone;
+	public AudioHelm.HelmController Lead;
 
 	public AudioHelm.Sequencer KickSeq;
 	public AudioHelm.Sequencer SnareSeq;
 	public AudioHelm.Sequencer HihatSeq;
 	public AudioHelm.Sequencer ArpSeq;
 	public AudioHelm.Sequencer AirDroneSeq;
+	public AudioHelm.Sequencer LeadSeq;
 
 	public int KickNote = 60;
 	public int SnareNote = 50;
@@ -34,6 +36,11 @@ public class HelmManagerScript : MonoBehaviour {
 	public int AirDroneNote2 = 98;
 	public int AirDroneNote3 = 100;
 
+	public int LeadNote1 = 93;
+	public int LeadNote2 = 98;
+	public int LeadNote3 = 91;
+	public int LeadNote4 = 88;
+
 	//Drone	
 	public float formantY = 0.0f; // variable example to change a synth parameter
 	public float formantX = 0.0f; // variable example to change a synth parameter
@@ -42,26 +49,33 @@ public class HelmManagerScript : MonoBehaviour {
 	public float stutterTempo = 0.0f; // variable example to change a synth parameter
 	public float stutterResampleTempo = 1.0f; // variable example to change a synth parameter
 
+	public float time = 1.5f;
+
 	// Use this for initialization
 	void Start () {
-		DrumsTriggerOnEnter ();
-		BassTriggerOnEnter ();
-		DroneTriggerOnEnter ();
-		ArpTriggerOnEnter ();
-		AirDroneTriggerOnEnter ();
+		//string[] notes = { "C-2", "Db-2", "D-2", "Eb-2", "E-2", "F-2", "Gb-2", "G-2", "Ab-2","A-2","Bb-2","B-2","C-1", "Db-1", "D-1", "Eb-1", "E-1", "F-1", "Gb-1", "G-1", "Ab-1","A-1","Bb-1","B-1","C0", "Db0", "D0", "Eb0", "E0", "F0", "Gb0", "G0", "Ab0","A0","Bb0","B0","C1", "Db1", "D1", "Eb1", "E1", "F1", "Gb1", "G1", "Ab1","A1","Bb1","B1","C2", "Db2", "D2", "Eb2", "E2", "F2", "Gb2", "G2", "Ab2","A2","Bb2","B2","C3", "Db3", "D3", "Eb3", "E3", "F3", "Gb3", "G3", "Ab3","A3","Bb3","B3","C4", "Db4", "D4", "Eb4", "E4", "F4", "Gb4", "G4", "Ab4","A4","Bb4","B4","C5", "Db5", "D5", "Eb5", "E5", "F5", "Gb5", "G5", "Ab5","A5","Bb5","B5","C6", "Db6", "D6", "Eb6", "E6", "F6", "Gb6", "G6", "Ab6","A6","Bb6","B6"};
+		Invoke("DroneEnable", time);
+		Invoke("AirDroneEnable", time*4);
+		Invoke("BassEnable", time*8);
+		Invoke("DrumsEnable", time*12);
+		Invoke("ArpEnable", time*16);
+		Invoke ("LeadEnable", time * 20);
 	}
-	// Update is called once per frame
-	void Update () {
-		//Drone
-		Drone.SetParameterPercent(AudioHelm.Param.kFormantY, formantY);
-		Drone.SetParameterPercent(AudioHelm.Param.kFormantX, formantX);
-		Drone.SetParameterPercent(AudioHelm.Param.kFilterBlend, filterBlend);
-		//Arp
-		Arp.SetParameterValue(AudioHelm.Param.kStutterTempo, stutterTempo);
-		Arp.SetParameterValue(AudioHelm.Param.kStutterResampleTempo, stutterResampleTempo);
+	void DroneEnable()
+	{
+		Drone.NoteOn (DroneNote1,1.0f);
+		Drone.NoteOn (DroneNote2,1.0f);
+		Drone.NoteOn (DroneNote3,1.0f); 
+		Drone.NoteOn (DroneNote4,1.0f);
 	}
-
-	void DrumsTriggerOnEnter()
+	void AirDroneEnable()
+	{
+		AirDroneSeq.Clear ();
+		AirDroneSeq.AddNote (AirDroneNote1, 0, 17);
+		AirDroneSeq.AddNote (AirDroneNote2, 0, 17);
+		AirDroneSeq.AddNote (AirDroneNote3, 0, 17);
+	}
+	void DrumsEnable()
 	{
 		KickSeq.Clear ();
 		SnareSeq.Clear ();
@@ -83,22 +97,12 @@ public class HelmManagerScript : MonoBehaviour {
 		HihatSeq.AddNote (HihatNote, 14, 15);
 
 	}
-	void BassTriggerOnEnter()
+	void BassEnable()
 	{
-		Bass.AllNotesOff ();
 		Bass.NoteOn (BassNote);
+		Bass.NoteOn (BassNote+12);
 	}
-
-	void DroneTriggerOnEnter()
-	{
-		Drone.AllNotesOff ();
-		Drone.NoteOn (DroneNote1);
-		Drone.NoteOn (DroneNote2);
-		Drone.NoteOn (DroneNote3); 
-		Drone.NoteOn (DroneNote4);
-	}
-
-	void ArpTriggerOnEnter()
+	void ArpEnable()
 	{
 		ArpSeq.Clear ();
 		ArpSeq.AddNote (ArpNote1, 0, 17);
@@ -106,12 +110,22 @@ public class HelmManagerScript : MonoBehaviour {
 		ArpSeq.AddNote (ArpNote3, 0, 17);
 		ArpSeq.AddNote (ArpNote4, 0, 17);
 	}
-
-	void AirDroneTriggerOnEnter()
+	void LeadEnable()
 	{
-		AirDroneSeq.Clear ();
-		AirDroneSeq.AddNote (AirDroneNote1, 0, 17);
-		AirDroneSeq.AddNote (AirDroneNote2, 0, 17);
-		AirDroneSeq.AddNote (AirDroneNote3, 0, 17);
+		LeadSeq.Clear ();
+		LeadSeq.AddNote (LeadNote1, 0, 1);
+		LeadSeq.AddNote (LeadNote2, 5, 6);
+		LeadSeq.AddNote (LeadNote3, 8, 9);
+		LeadSeq.AddNote (LeadNote4, 13, 14);
+	}
+	// Update is called once per frame
+	void Update () {
+		//Drone
+		Drone.SetParameterPercent(AudioHelm.Param.kFormantY, formantY);
+		Drone.SetParameterPercent(AudioHelm.Param.kFormantX, formantX);
+		Drone.SetParameterPercent(AudioHelm.Param.kFilterBlend, filterBlend);
+		//Arp
+		Arp.SetParameterValue(AudioHelm.Param.kStutterTempo, stutterTempo);
+		Arp.SetParameterValue(AudioHelm.Param.kStutterResampleTempo, stutterResampleTempo);
 	}
 }
