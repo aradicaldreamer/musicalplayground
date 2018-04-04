6c8f3c357736d1f53b832a2d76286221276ae24e
Fletchbot <al-fletch@hotmail.co.uk>
2018-04-04 15:29:56 +0100

    added OSC scripts

---
+++ ProjectionPrototype/Assets/Scripts/OscOutputManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OscOutputManager : MonoBehaviour {
+	public OSC osc;
+	public GameObject Manager;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HelmManagerScript hms = Manager.GetComponent<HelmManagerScript>();
+		OscMessage message = new OscMessage();
+		message.address = "/wek/inputs";
+	//	message.values.Add(hms.AirDroneOn);
+		message.values.Add(hms.DroneX);
+		message.values.Add(hms.DroneY);
+		message.values.Add(hms.DroneFeedback);
+		message.values.Add(hms.DroneMod);
+		osc.Send(message);
+	}
+}
