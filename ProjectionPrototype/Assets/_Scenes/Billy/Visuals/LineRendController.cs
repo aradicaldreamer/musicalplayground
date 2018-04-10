8fe8fb396e5ae9442bbd2c5f5524faa6db23fce5
Billy-Desktop <wkarnchanapee@gmail.com>
2018-04-10 22:09:49 +0100

    Unused linerenderer script

---
+++ ProjectionPrototype/Assets/_Scenes/Billy/Visuals/LineRendController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRendController : MonoBehaviour {
+
+    public int vertexCount = 40;
+    public float lineWidth = 0.2f;
+    public float radius;
+    public bool circleFillscreen;
+
+    LineRenderer lineRenderer;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    
+    private void SetupCircle()
+    {
+        lineRenderer.widthMultiplier = lineWidth;
+
+        if (circleFillscreen)
+        {
+            radius = Vector3.Distance(Camera.main.ScreenToWorldPoint(new Vector3(0f, Camera.main.pixelRect.yMax, 0f)), 
+                Camera.main.ScreenToWorldPoint(new Vector3(0f, Camera.main.pixelRect.yMin, 0f))) * 0.5f - lineWidth;
+        }
+
+        float deltaTheta = (2f * Mathf.PI) / vertexCount;
+        float theta = 0f;
+
+        lineRenderer.positionCount = vertexCount;
+        for (int i = 0; i < lineRenderer.positionCount; i++)
+        {
+            Vector3 pos = new Vector3(radius * Mathf.Cos(theta), radius * Mathf.Sin(theta), 0f);
+            lineRenderer.SetPosition(i, pos);
+            theta += Time.deltaTime;
+        }
+        
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        float deltaTheta = (2f * Mathf.PI) / 40f;
+        float theta = 0f;
+
+        Vector3 oldPos = Vector3.zero;
+
+        for (int i = 0; i < vertexCount + 1; i++)
+        {
+            Vector3 pos = new Vector3(radius * Mathf.Cos(theta), radius * Mathf.Sin(theta), 0f);
+            Gizmos.DrawLine(oldPos, transform.position + pos);
+            oldPos = transform.position + pos;
+
+            theta += deltaTheta;
+        }
+    }
+#endif
+}
