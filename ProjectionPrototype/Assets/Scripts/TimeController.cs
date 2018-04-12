e06d45fc03f12cc995d1db1df69bffcc75ec8c8c
aradicaldreamer <31594961+aradicaldreamer@users.noreply.github.com>
2018-04-12 18:46:16 +0100

    Reset idCounter before reaching Int limit

--- ProjectionPrototype/Assets/Scripts/TimeController.cs
+++ ProjectionPrototype/Assets/Scripts/TimeController.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 
 public class TimeController : MonoBehaviour {
 
 
-	public Stopwatch timer = new Stopwatch();
+
 	public int idCounter = 0;
 
 	public static TimeController main;
@@ -14,11 +13,15 @@ public class TimeController : MonoBehaviour {
 	// Use this for initialization
 	void Awake () {
 		main = this;
-		timer.Start ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		// the maximum value of an integer 2147483647
+
+		if (idCounter > 2147483647 - 1) {
+			idCounter = 0;
+		}
 	}
 }
